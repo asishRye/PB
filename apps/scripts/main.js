bb78fb9f1665550233b708e4730c49d7e3c65555
Asish Binu Mathew <asish@accubits.com>
2021-03-22 15:06:08 +0530

    minor changes

--- apps/scripts/main.js
+++ apps/scripts/main.js
@@ -1,9 +1,16 @@
 const electron = require("electron");
 const { ipcRenderer } = electron;
 const runAutomation = document.getElementById("runAutomation");
+const terminateAutomation = document.getElementById("terminateAutomation");
 
 runAutomation.onclick = (e) => {
     // event.preventDefault();
     ipcRenderer.send("runEvent");
-    console.log("Something happened");
+    console.log("Event running");
+};
+
+terminateAutomation.onclick = (e) => {
+    // event.preventDefault();
+    ipcRenderer.send("terminateProcess");
+    console.log("Event terminating");
 };
--- launcher.js
+++ launcher.js
@@ -1,9 +1,10 @@
 const { app, ipcMain } = require("electron");
 const electron = require(`electron`);
-const exec = require('child_process').exec;
+const { exec, spawn, fork } = require('child_process');
 
 const { BrowserWindow, ipcRenderer, Menu } = electron;
 
+var processVar;
 let mainWindow;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
@@ -17,11 +18,13 @@ app.on('ready', () => {
 });
 
 ipcMain.on('runEvent', (event) => {
-    console.log("Event caught");
 
-    exec('cd D:\\accubits\\Electron\\PB\\core\\tagui && tagui D:\\accubits\\Electron\\PB\\core\\tagui\\amazon.tag ', (err, stdout, stderr) => {
-        console.log(stdout);
-        console.log(err);
-        console.log(stderr);
-    });
+    // a = spawn('cd D:\\accubits\\Electron\\PB\\core\\tagui && tagui D:\\accubits\\Electron\\PB\\core\\tagui\\amazon.tag', { detached: true });
+    a = spawn("tagui", ['D:\\accubits\\Electron\\PB\\core\\tagui\\amazon.tag'], { detached: true, env: process.env.PATH }).on('error', function (err) { throw err; });
+});
+
+ipcMain.on('terminateProcess', (event) => {
+    // a.kill("SIGINT");
+    processVar.kill();
+    console.log("Process Killed");
 });
